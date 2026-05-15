f37ac33625ac2629170366f6a46791f366fd7f09
sannan abbsi <sannanabbasi34@gmail.com>
2026-05-15 13:31:47 +0500

    message

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+// If you are using the babel plugin from your screenshot:
+import babel from '@rollway/plugin-babel' 
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/gonme-game/", // This must match your repo name exactly
+  base: "/gonme-game",
 })
